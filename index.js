ec6a8528cf94bcaa896ccb20eb0736728cde9b58
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-05-18 23:39:26 +0600

    (FINAL): Randomly quote generation from a file project has been done

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+import quotes from "./lib/quotes.js";
 import math from "./lib/randomNum.js";
 
 const app = {};
 
-console.log(math.getRandomNumber(1,10));
+app.printQuote = () => {
+    const quotesArray = quotes.getAllQuotes();
+    const randomNumber = math.getRandomNumber(1, quotesArray.length);
+
+    const quote = quotesArray[randomNumber - 1];
+    console.log(quote);
+}
+
+app.inifinitLoop = () => {
+    setInterval(app.printQuote, 1000);
+}
+
+
+app.inifinitLoop();
